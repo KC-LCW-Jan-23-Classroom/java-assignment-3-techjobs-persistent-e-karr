080cc124f7a091d842a3ebc8f1a35d5b72a62971
Elizabeth Karr <e.karr93@icloud.com>
2023-09-06 20:29:39 -0500

    parts 1-3

--- queries.sql
+++ queries.sql
@@ -1,7 +1,15 @@
 -- Part 1: Test it with SQL
+-- CREATE TABLE job (
+    -- id int PRIMARY KEY AUTO_INCREMENT
+    -- name varchar(255)
+    -- employer varchar(255)
+    -- skills varchar(255)
+    -- );
 
 -- Part 2: Test it with SQL
+--    SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
+-- DROP TABLE job;
 
 -- Part 4: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,6 +1,10 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,12 +12,19 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -27,23 +38,33 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("employers", employerRepository.findAll());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
-
+                                       Errors errors, Model model, @RequestParam int employerId) {
+//        @RequestParam List<Integer> skills
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
+            model.addAttribute("employers", employerRepository.findAll());
             return "add";
         }
+
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
+        Employer employer = optEmployer.get();
+        newJob.setEmployer(employer);
+        jobRepository.save(newJob);
 
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
+        Optional<Job> optJob = jobRepository.findById(jobId);
+        Job job = optJob.get();
+        model.addAttribute("job", job);
 
         return "view";
     }
